c29e103076120102e0d05bfe8ef6d587cc5e8b67
ArneD4 <arne.defoor@gmail.Com>
2020-11-17 09:14:49 +0100

    updated

--- JAVASCRIPT/excercise&games/mathGame/app.js
+++ JAVASCRIPT/excercise&games/mathGame/app.js
@@ -11,6 +11,7 @@ var answer = document.getElementById('answer');
 
 
 
+
 question.innerHTML = number1 + operator + number2;
 
 
@@ -19,35 +20,41 @@ console.log(operator)
 console.log(number2)
 
 
+// | replaced by eval() |
+// V                    V
 
-function calc(){
-    if(operator === "+"){
-        return number1 + number2
-    }else if (operator === "-"){
-        return number1 - number2
-    }else if (operator === "*"){
-       return number1 * number2
-    }else{
-        return number1 / number2
-    }  
-}
+// function calc(){
+//     if(operator === "+"){
+//         return number1 + number2
+//     }else if (operator === "-"){
+//         return number1 - number2
+//     }else if (operator === "*"){
+//        return number1 * number2
+//     }else{
+//         return number1 / number2
+//     }  
+// }
 
-result = calc();
-console.log(result)
-console.log(answer)
+// result = calc();
 
 
 function calcCheck(){
-    console.log(Number(answer.value))
-    // if(result == answer){
-    //     console.log('correct')    
-    // }else{
-    //     console.log('wrong')  
-    // }
+    console.log(Number(answer.value)) //this one
+   // console.log(answer.value) not this one
+    let correct_answer = eval(number1 + operator + number2)
+    console.log(correct_answer)
+    if(correct_answer == Number(answer.value)){
+        console.log('correct')    
+        correct.innerHTML++
+        
+    }else{
+        console.log('wrong')  
+        incorrect.innerHTML++
+        
+    }
+    
 }
 
 
-
-
-
+check.addEventListener('click',calcCheck)
 
