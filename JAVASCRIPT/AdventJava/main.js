0c3736f758a3cdbca6a7dc1ada3cdfd53b1ff41f
ArneD4 <arne.defoor@gmail.Com>
2020-12-08 12:45:25 +0100

    updated

--- JAVASCRIPT/AdventJava/main.js
+++ JAVASCRIPT/AdventJava/main.js
@@ -7,8 +7,6 @@ for (let i=0; i<numbers.length;i++){
     for(let j = 0; j<numbers.length;j++){
     let b = numbers[j]
     if(a + b === 2020){
-        console.log(a)
-        console.log(b)
         let result = a*b
         console.log(result)
     }
