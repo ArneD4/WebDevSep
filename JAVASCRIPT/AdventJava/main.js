dd1a23f4e27aafe90d5c6130215b60e71999f3d2
ArneD4 <arne.defoor@gmail.Com>
2020-12-08 12:42:56 +0100

    updated

---
+++ JAVASCRIPT/AdventJava/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+
+for (let i=0; i<numbers.length;i++){
+    let a = numbers[i];
+    for(let j = 0; j<numbers.length;j++){
+    let b = numbers[j]
+    if(a + b === 2020){
+        console.log(a)
+        console.log(b)
+        let result = a*b
+        console.log(result)
+    }
+    }
+}
+
+
+
