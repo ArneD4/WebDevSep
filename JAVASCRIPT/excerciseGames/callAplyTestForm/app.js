b0fd49a77cd77975f13d33e8633699bce32a2156
ArneD4 <arne.defoor@gmail.Com>
2020-12-10 11:43:54 +0100

    updated

--- JAVASCRIPT/excerciseGames/callAplyTestForm/app.js
+++ JAVASCRIPT/excerciseGames/callAplyTestForm/app.js
@@ -12,6 +12,7 @@ const userMgmt = {
     addUser:function(firstname,lastname,email,street,city,zipcode,birthdate,phone,pass,confrim_pass,term,country,gender,country_code){
     let user = `FIRSTNAME: ${this.firstname}\nLASTNAME: ${this.lastname}\nEMAIL: ${this.email}\nSTREET: ${this.street}\nCITY: ${this.city}\nZIPCODE: ${this.zipcode}\nBIRTHDATE: ${this.birthdate}\nPHONE: ${this.phone}\nPASSWORD: ${this.pass}\nCONFIRM_PASSWORD: ${this.confirm_pass}\nTERM: ${this.term}\nCOUNTRY: ${this.country}\nGENDER: ${this.gender}`
     console.log(user)
+    result.innerText = user
     }
 }
 
@@ -38,10 +39,10 @@ submitBtn.addEventListener('click',function(e){
        
         console.log(formObj)
         userMgmt.addUser.apply(formObj)
+        
     }else{
         console.log('not accepted')
     }
-    console.log(formDatas)
     e.preventDefault();
 
 });
