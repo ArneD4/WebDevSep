3069ca4387ad505fc8979eb740ce5ff3bc899735
ArneD4 <arne.defoor@gmail.Com>
2021-01-28 16:10:36 +0100

    updated

---
+++ REACTJS/EXCERCISES/userform/src/components/showUsers.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Container,Row,Col,Button,Input} from 'reactstrap'
+
+export default class showUsers extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
